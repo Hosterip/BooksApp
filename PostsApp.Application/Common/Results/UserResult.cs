a6b1bbb6ef2a08ae29860857932870f3c4344570
hosterip <hosteripwork@gmail.com>
2024-01-19 15:59:04 +0200

    moved user result and renamed it

---
+++ PostsApp.Application/Common/Results/UserResult.cs
@@ -0,0 +1,6 @@
+namespace PostsApp.Application.Common.Results;
+
+public class UserResult
+{
+    public string username { get; set; }
+}
--- PostsApp.Application/Users/Results/DefaultUserResult.cs
+++
@@ -1,6 +0,0 @@
-namespace PostsApp.Application.Users.Results;
-
-public class DefaultUserResult
-{
-    public string username { get; set; }
-}
