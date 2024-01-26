2b9602703ef277fbfafd7ff2aabeff7072ebe064
hosterip <hosteripwork@gmail.com>
2024-01-27 00:02:48 +0200

    misc

--- PostsApp.Application/Common/Interfaces/IUserRepository.cs
+++ PostsApp.Application/Common/Interfaces/IUserRepository.cs
@@ -6,5 +6,4 @@ namespace PostsApp.Application.Common.Interfaces;
 public interface IUserRepository : IGenericRepository<User>
 {
     Task<PaginatedArray<UserResult>> GetPaginated(int page, int limit, string query);
-    Task<bool> IsUsernameUnique(string username);
 }
--- PostsApp.Infrastructure/Implementation/UserRepository.cs
+++ PostsApp.Infrastructure/Implementation/UserRepository.cs
@@ -1,4 +1,3 @@
-using Microsoft.EntityFrameworkCore;
 using PostsApp.Application.Common.Extensions;
 using PostsApp.Application.Common.Interfaces;
 using PostsApp.Application.Common.Results;
@@ -9,9 +8,7 @@ namespace PostsApp.Infrastructure.Implementation;
 
 public class UserRepository : GenericRepository<User>, IUserRepository
 {
-    public UserRepository(AppDbContext dbContext) : base(dbContext)
-    {
-    }
+    public UserRepository(AppDbContext dbContext) : base(dbContext) { }
 
     public async Task<PaginatedArray<UserResult>> GetPaginated(int page, int limit, string query)
     {
@@ -21,10 +18,5 @@ public class UserRepository : GenericRepository<User>, IUserRepository
                 where query == null || user.Username.Contains(query)
                 select new UserResult{Username = user.Username})
             .PaginationAsync(page, limit);
-    }
-
-    public async Task<bool> IsUsernameUnique(string username)
-    {
-        return !await _dbContext.Users.AnyAsync(user => user.Username == username);
     }
 }
