fa258325fdbf03686c305af6d683f484c1de2561
hosterip <hosteripwork@gmail.com>
2024-02-20 02:14:08 +0200

    Mock data for tests

---
+++ tests/BooksApp.Application.UnitTests/MockData/MockRole.cs
@@ -0,0 +1,13 @@
+﻿using PostsApp.Domain.Constants;
+using PostsApp.Domain.Models;
+
+namespace Application.UnitTest.MockData;
+
+public static class MockRole
+{
+    public static Role GetRole(string role) => new Role
+    {
+        Id = 0,
+        Name = role
+    };
+}
---
+++ tests/BooksApp.Application.UnitTests/MockData/MockUser.cs
@@ -0,0 +1,16 @@
+﻿using PostsApp.Domain.Constants;
+using PostsApp.Domain.Models;
+
+namespace Application.UnitTest.MockData;
+
+public static class MockUser
+{
+    public static User GetUser(string? role) => new User
+    {
+        Id = 0,
+        Username = "Hello world",
+        Hash = "Hello world",
+        Salt = "Hello world",
+        Role = MockRole.GetRole(role ?? RoleConstants.Member)
+    };
+}
