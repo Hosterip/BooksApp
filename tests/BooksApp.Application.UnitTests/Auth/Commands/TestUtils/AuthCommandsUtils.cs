a7ee40a208359527cb59ec4dafafbd0fd7fa9da2
hosterip <hosteripwork@gmail.com>
2024-02-26 23:04:57 +0200

    Utils for auth

--- tests/BooksApp.Application.UnitTests/Auth/Commands/TestUtils/AuthCommandsUtils.cs
+++ tests/BooksApp.Application.UnitTests/Auth/Commands/TestUtils/AuthCommandsUtils.cs
@@ -1,14 +1,34 @@
-﻿using PostsApp.Application.Auth.Commands.ChangePassword;
+﻿using System.Linq.Expressions;
+using Application.UnitTest.Auth.TestUtils.Constants;
+using Application.UnitTest.TestUtils.MockData;
+using Moq;
+using PostsApp.Application.Auth.Commands.ChangePassword;
+using PostsApp.Application.Common.Interfaces;
+using PostsApp.Domain.Models;
 
-namespace Application.UnitTest.Books.Commands.CreateBookCommandTests.TestUtils;
+namespace Application.UnitTest.Auth.Commands.TestUtils;
 
 public static class AuthCommandsUtils
 {
-    public static ChangePasswordCommand ChangePasswordCommand => 
+    public static ChangePasswordCommand ChangePasswordCommandCorrect => 
         new ChangePasswordCommand
         {
             Id = 1,
-            NewPassword = "1",
-            OldPassword = "2"
+            NewPassword = PasswordConstants.MockPassword,
+            OldPassword = PasswordConstants.CorrectPassword
         };
+    public static ChangePasswordCommand ChangePasswordCommandIncorrect => 
+        new ChangePasswordCommand
+        {
+            Id = 1,
+            NewPassword = PasswordConstants.MockPassword,
+            OldPassword = PasswordConstants.IncorrectPassword
+        };
+
+    public static void SetupUsersGetSingleWhereAsync(Mock<IUnitOfWork> mockUnitOfWork)
+    {
+        mockUnitOfWork.Setup(x => x.Users.GetSingleWhereAsync(
+                It.IsAny<Expression<Func<User, bool>>>()))
+            .ReturnsAsync(MockUser.GetUser(null, HashSaltConstants.Hash, HashSaltConstants.Salt));
+    }
 }
