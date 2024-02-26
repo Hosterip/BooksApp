d3a672753dd3fefa3d5996484dee683603ddd1ef
hosterip <hosteripwork@gmail.com>
2024-02-26 23:13:27 +0200

    removed data annotation from user model

--- src/BooksApp.Domain/Models/User.cs
+++ src/BooksApp.Domain/Models/User.cs
@@ -1,14 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace PostsApp.Domain.Models;
+﻿namespace PostsApp.Domain.Models;
 
 public class User
 {
     public int Id { get; set; }
     public string Username { get; set; }
     public Role Role { get; set; }
-    [Required]
     public string Hash { get; set; }
-    [Required]
     public string Salt { get; set; }
 }
