02784fb468a99c7ca2e30bee5d65a3d162daf91b
hosterip <hosteripwork@gmail.com>
2024-06-15 23:13:03 +0300

    new Value Object

---
+++ src/BooksApp.Domain/User/ValueObjects/UserId.cs
@@ -0,0 +1,23 @@
+﻿using PostsApp.Domain.Common.Models;
+
+namespace PostsApp.Domain.User.ValueObjects;
+
+public sealed class UserId : ValueObject
+{
+    public Guid Value { get; }
+
+    public UserId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static UserId CreateUserId()
+    {
+        return new(Guid.NewGuid());
+    }
+    
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
