968578edf000cb7baf981521504e3cc9e1295203
hosterip <hosteripwork@gmail.com>
2024-06-15 23:11:18 +0300

    Base aggregate root pattern models

---
+++ src/BooksApp.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿namespace PostsApp.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId> where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+}
