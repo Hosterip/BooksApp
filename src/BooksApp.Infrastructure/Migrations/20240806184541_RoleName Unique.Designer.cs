0364574853294320c988da25539dadec86a2bc30
Hosterip <hosteripwork@gmail.com>
2024-08-06 21:46:11 +0300

    Migration for previous commit

--- src/BooksApp.Infrastructure/Migrations/20240806184541_RoleName Unique.Designer.cs
+++ src/BooksApp.Infrastructure/Migrations/20240806184541_RoleName Unique.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PostsApp.Infrastructure.Data;
 
 #nullable disable
 
-namespace PostsApp.Infrastructure.Data.Migrations
+namespace PostsApp.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240806184541_RoleName Unique")]
+    partial class RoleNameUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
