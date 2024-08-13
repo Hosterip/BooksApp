9f5172daf17eea4c34aab51d3f0bd628f88e642b
Hosterip <hosteripwork@gmail.com>
2024-08-13 21:28:48 +0300

    fixed GetPaginatedBookshelfBooks method

--- src/BooksApp.Infrastructure/Implementation/Repositories/BooksRepository.cs
+++ src/BooksApp.Infrastructure/Implementation/Repositories/BooksRepository.cs
@@ -57,36 +57,36 @@ public class BooksRepository : GenericRepository<Book>, IBooksRepository
 
     public async Task<PaginatedArray<BookResult>?>? GetPaginatedBookshelfBooks(Guid bookshelfId, int limit, int page)
     {
-        if (!await AnyById(bookshelfId)) return null;
-        var bookshelf = await _dbContext.Bookshelfes
+        if (!await _dbContext.Bookshelfes
+                .AnyAsync(bookshelf => bookshelf.Id == BookshelfId.CreateBookshelfId(bookshelfId))) return null;
+        var result = await _dbContext.Bookshelfes
             .Include(bookshelf => bookshelf.BookshelfBooks)
-            .SingleAsync(bookshelf => bookshelf.Id == BookshelfId.CreateBookshelfId(bookshelfId));
-        var books = await 
-            (
-                from bookshelfBook in bookshelf.BookshelfBooks
-                let book = bookshelfBook.Book
-                let user = new UserResult
+            .Where(bookshelf => bookshelf.Id == BookshelfId.CreateBookshelfId(bookshelfId))
+            .SelectMany(bookshelf => bookshelf.BookshelfBooks)
+            .Select(bb =>  new BookResult
                 {
-                    Id = book.Author.Id.Value.ToString(),
-                    Username = book.Author.Username,
-                    Role = book.Author.Role.Name,
-                    AvatarName = book.Author.Avatar.ImageName
-                }
-                select new BookResult
-                {
-                    Id = book.Id.Value.ToString(),
-                    Title = book.Title,
-                    Description = book.Description,
-                    Author = user,
-                    Average = AverageRating(book.Id.Value),
-                    CoverName = book.Cover.ImageName,
-                    Genres = book.Genres.Select(genre => new GenreResult { Id = genre.Id.Value, Name = genre.Name })
+                    Id = bb.Book.Id.Value.ToString(),
+                    Title = bb.Book.Title,
+                    Description = bb.Book.Description,
+                    Author = new UserResult
+                    {
+                        Id = bb.Book.Author.Id.Value.ToString(),
+                        Username = bb.Book.Author.Username,
+                        AvatarName = bb.Book.Author.Avatar == null ? null : bb.Book.Author.Avatar.ImageName,
+                        Role = bb.Book.Author.Role.Name
+                    },
+                    Average = -1,
+                    CoverName = bb.Book.Cover.ImageName,
+                    Genres = bb.Book.Genres.Select(genre => new GenreResult { Id = genre.Id.Value, Name = genre.Name })
                         .ToList()
                 }
-            )
-            .AsQueryable()
-            .PaginationAsync(page, limit);
-        return books;
+            ).PaginationAsync(page, limit);
+        result.Items = result.Items.Select(book =>
+        {
+            book.Average = AverageRating(new Guid(book.Id));
+            return book;
+        }).ToArray();
+        return result;
 
     }
 
