0601e35af510f768aaf2b5335d610b102a8b60ac
Hosterip <hosteripwork@gmail.com>
2024-10-02 18:47:50 +0200

    little changes

--- src/BooksApp.API/Common/Contracts/Responses/User/UserResponse.cs
+++ src/BooksApp.API/Common/Contracts/Responses/User/UserResponse.cs
@@ -3,7 +3,9 @@ namespace PostsApp.Common.Contracts.Responses.User;
 public class UserResponse
 {
     public required string Id { get; init; }
-    public required string Username { get; init; }
+    public required string FirstName { get; init; }
+    public required string? MiddleName { get; init; }
+    public required string? LastName { get; init; }
     public required string Role { get; init; }
     public required string? AvatarName { get; init; }
 }
--- src/BooksApp.Application/Common/Constants/ValidationMessages/AuthValidationMessages.cs
+++ src/BooksApp.Application/Common/Constants/ValidationMessages/AuthValidationMessages.cs
@@ -3,7 +3,7 @@ namespace PostsApp.Application.Common.Constants.Exceptions;
 public static class AuthValidationMessages
 {
     public const string NotFound = "User not found";
-    public const string Occupied = "Username is occupied";
+    public const string Occupied = "Email is occupied";
     public const string Password = "Password is wrong";
     public const string UsernameOrPassword = "Username or password is wrong";
 }
