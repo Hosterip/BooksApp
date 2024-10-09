541e2ccfc437622cd94e248f6a2f8251d35e4a4d
Hosterip <hosteripwork@gmail.com>
2024-10-09 17:08:59 +0200

    new validation messages

---
+++ src/BooksApp.Application/Common/Constants/ValidationMessages/GenreValidationMessages.cs
@@ -0,0 +1,6 @@
+namespace PostsApp.Application.Common.Constants.ValidationMessages;
+
+public static class GenreValidationMessages
+{
+    public const string AlreadyExists = "This genre is already exists";
+}
--- src/BooksApp.Application/Common/Constants/ValidationMessages/RoleValidationMessages.cs
+++ src/BooksApp.Application/Common/Constants/ValidationMessages/RoleValidationMessages.cs
@@ -3,4 +3,5 @@ namespace PostsApp.Application.Common.Constants.Exceptions;
 public static class RoleValidationMessages
 {
     public const string NotFound = "Role not found";
+    public const string CanNotChangeYourOwn = "You can't change your own role";
 }
