37eac11447607bd313f866614c961657269f2f10
Hosterip <hosteripwork@gmail.com>
2024-10-14 22:24:12 +0200

    new property for better validation

--- src/BooksApp.Domain/Common/Constants/DefaultBookshelvesNames.cs
+++ src/BooksApp.Domain/Common/Constants/DefaultBookshelvesNames.cs
@@ -5,4 +5,11 @@ public static class DefaultBookshelvesNames
     public const string CurrentlyReading = "currently-reading";
     public const string ToRead = "to-read";
     public const string Read = "read";
+
+    public static readonly HashSet<string> AllValues =
+    [
+        Read,
+        ToRead,
+        CurrentlyReading
+    ];
 }
