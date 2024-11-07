4fd28446990d448288cee11f5ff4a32f113da8f4
Hosterip <hosteripwork@gmail.com>
2024-11-07 20:46:24 +0100

    new methods in users repository

--- src/BooksApp.Application/Common/Interfaces/Repositories/IUsersRepository.cs
+++ src/BooksApp.Application/Common/Interfaces/Repositories/IUsersRepository.cs
@@ -7,9 +7,12 @@ namespace BooksApp.Application.Common.Interfaces.Repositories;
 public interface IUsersRepository : IGenericRepository<User>
 {
     Task<PaginatedArray<UserResult>> GetPaginated(Guid? currentUserId, int page, int limit, string query);
+    Task<PaginatedArray<UserResult>> GetPaginatedFollowers(Guid? currentUserId, Guid userId, int page, int limit, string query);
     Task<User?> GetSingleById(Guid guid);
     Task<bool> AnyById(Guid guid);
     Task<bool> AnyByEmail(string email);
     Task<bool> AnyFollower(Guid userId, Guid followerId);
     Task AddFollower(Guid userId, Guid followerId);
+    Task RemoveFollower(Guid userId, Guid followerId);
+    Task<int> CountFollowers(Guid userId);
 }
--- src/BooksApp.Infrastructure/Implementation/Repositories/UsersRepository.cs
+++ src/BooksApp.Infrastructure/Implementation/Repositories/UsersRepository.cs
@@ -47,6 +47,45 @@ public class UsersRepository : GenericRepository<User>, IUsersRepository
             .PaginationAsync(page, limit);
     }
 
+    public async Task<PaginatedArray<UserResult>> GetPaginatedFollowers(
+        Guid? currentUserId,
+        Guid userId,
+        int page,
+        int limit,
+        string query)
+    {
+        
+        return await (
+                from relationship in _dbContext.Relationships
+                    .AsNoTracking()
+                    .Where(x => x.UserId == UserId.CreateUserId(userId))
+                    .Include(x => x.UserId)
+                    .Include(x => x.FollowerId)
+                join user in _dbContext.Users
+                        .Include(x => x.Followers)
+                        .Include(x => x.Following)
+                    on relationship.FollowerId equals user.Id  
+                where query == null || user.FirstName.Contains(query)
+                let viewerRelationship = user.ViewerRelationship(currentUserId)
+                select new UserResult
+                {
+                    Id = user.Id.Value.ToString(),
+                    Email = user.Email,
+                    FirstName = user.FirstName,
+                    MiddleName = user.MiddleName,
+                    LastName = user.LastName,
+                    Role = user.Role.Name,
+                    AvatarName = user.Avatar.ImageName ?? null,
+                    ViewerRelationship = new ViewerRelationship
+                    {
+                        IsFollowing = viewerRelationship.IsFollowing,
+                        IsFriend = viewerRelationship.IsFriend,
+                        IsMe = viewerRelationship.IsMe
+                    }
+                })
+            .PaginationAsync(page, limit);
+    }
+
     public async Task<User?> GetSingleById(Guid guid)
     {
         return await _dbContext.Users
@@ -83,5 +122,24 @@ public class UsersRepository : GenericRepository<User>, IUsersRepository
         var follower = await GetSingleById(followerId);
         if (user != null && follower != null)
             user.AddFollower(follower);
+    }
+
+    public async Task RemoveFollower(Guid userId, Guid followerId)
+    {
+        var user = await GetSingleById(userId);
+        var follower = await GetSingleById(followerId);
+        if (user != null && follower != null)
+            user.RemoveFollower(follower);
+    }
+
+    public async Task<int> CountFollowers(Guid userId)
+    {
+        var parsedUserId = UserId.CreateUserId(userId);
+        return await _dbContext.Users
+            .Include(x => x.Followers)
+            .Take(1)
+            .Where(x => x.Id == parsedUserId)
+            .SelectMany(x => x.Followers)
+            .CountAsync();
     }
 }
