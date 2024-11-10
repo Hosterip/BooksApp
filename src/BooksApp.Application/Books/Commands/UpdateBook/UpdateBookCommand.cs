5acdb6ab212c7776fe926f62c6e77c95e90b6a83
Hosterip <hosteripwork@gmail.com>
2024-11-10 17:19:34 +0100

    required for request properties

--- src/BooksApp.Application/Books/Commands/UpdateBook/UpdateBookCommand.cs
+++ src/BooksApp.Application/Books/Commands/UpdateBook/UpdateBookCommand.cs
@@ -8,8 +8,8 @@ public class UpdateBookCommand : IRequest<BookResult>
 {
     public required Guid Id { get; init; }
     public required Guid UserId { get; init; }
-    public string? Title { get; init; }
-    public string? Description { get; init; }
+    public required string? Title { get; init; }
+    public required string? Description { get; init; }
     public required IFormFile? Image { get; init; }
     public required List<Guid> GenreIds { get; init; }
 }
