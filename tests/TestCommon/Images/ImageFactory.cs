33801307b35dd0bcb21bdbde570c2b4af71d75ab
Hosterip <hosteripwork@gmail.com>
2025-01-20 23:40:17 +0100

    new method to create in memory IFormFile

--- tests/TestCommon/Images/ImageFactory.cs
+++ tests/TestCommon/Images/ImageFactory.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using BooksApp.Domain.Image;
+using Microsoft.AspNetCore.Http;
 using TestCommon.Common.Constants;
 
 namespace TestCommon.Images;
@@ -8,5 +10,12 @@ public static class ImageFactory
     public static Image CreateImage(string imageName = Constants.Images.ImageName)
     {
         return Image.Create(imageName);
+    }
+
+    public static IFormFile CreateFormFileImage()
+    {
+        var bytes = Encoding.UTF8.GetBytes("foo");
+
+        return new FormFile(new MemoryStream(bytes), 0, bytes.Length, "Data", "foo.png");
     }
 }
