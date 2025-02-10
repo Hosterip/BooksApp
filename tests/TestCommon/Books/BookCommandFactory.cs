f8036a34b33893b9ec50a02f433040ba4755076b
Hosterip <hosteripwork@gmail.com>
2025-02-10 23:36:39 +0100

    implemented creation of all book's commands

--- tests/TestCommon/Books/BookCommandFactory.cs
+++ tests/TestCommon/Books/BookCommandFactory.cs
@@ -1,4 +1,7 @@
 using BooksApp.Application.Books.Commands.CreateBook;
+using BooksApp.Application.Books.Commands.DeleteBook;
+using BooksApp.Application.Books.Commands.PrivilegedDeleteBook;
+using BooksApp.Application.Books.Commands.UpdateBook;
 using Microsoft.AspNetCore.Http;
 using TestCommon.Common.Constants;
 using TestCommon.Genres;
@@ -22,6 +25,49 @@ public static class BookCommandFactory
             Description = description,
             GenreIds = genres ?? [GenreFactory.CreateGenre().Id.Value],
             Image = image ?? ImageFactory.CreateFormFileImage()
+        };
+    }
+    
+    public static UpdateBookCommand CreateUpdateBookCommand(
+        Guid? bookId = null,
+        Guid? userId = null,
+        string title = Constants.Books.Title, 
+        string description = Constants.Books.Description,
+        List<Guid>? genres = null,
+        IFormFile? image = null)
+    {
+        return new UpdateBookCommand
+        {
+            Id = bookId ?? Guid.NewGuid(),
+            UserId = userId ?? Guid.NewGuid(),
+            Title = title,
+            Description = description,
+            GenreIds = genres ?? [GenreFactory.CreateGenre().Id.Value],
+            Image = image ?? ImageFactory.CreateFormFileImage()
+        };
+    }
+    
+    public static DeleteBookCommand CreateDeleteBookCommand(
+        Guid? bookId = null,
+        Guid? userId = null
+    )
+    {
+        return new DeleteBookCommand
+        {
+            UserId = userId ?? Guid.NewGuid(),
+            Id = bookId ?? Guid.NewGuid()
+        };
+    }
+    
+    public static PrivilegedDeleteBookCommand CreatePrivilegedDeleteBookCommand(
+        Guid? bookId = null,
+        Guid? userId = null
+    )
+    {
+        return new PrivilegedDeleteBookCommand
+        {
+            UserId = userId ?? Guid.NewGuid(),
+            Id = bookId ?? Guid.NewGuid()
         };
     }
 }
