c4e8e82e203d7d317deea0e64d564757dc09ec2e
Hosterip <hosteripwork@gmail.com>
2025-02-11 22:06:01 +0100

    made createImage return non-nullable string

--- src/BooksApp.Application/Common/Interfaces/IImageFileBuilder.cs
+++ src/BooksApp.Application/Common/Interfaces/IImageFileBuilder.cs
@@ -4,7 +4,7 @@ namespace BooksApp.Application.Common.Interfaces;
 
 public interface IImageFileBuilder
 {
-    Task<string?> CreateImage(IFormFile file, CancellationToken token = default);
+    Task<string> CreateImage(IFormFile file, CancellationToken token = default);
     bool DeleteImage(string fileName);
     (FileInfo fileInfo, FileStream fileStream) RetrieveImage(string fileName);
     bool AnyImage(string fileName);
