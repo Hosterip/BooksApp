2c9da085e410fc26d4e7846d42f42ee777a48546
Hosterip <hosteripwork@gmail.com>
2025-02-11 22:07:03 +0100

    misc

--- src/BooksApp.Application/Common/Constants/ValidationMessages/AuthValidationMessages.cs
+++ src/BooksApp.Application/Common/Constants/ValidationMessages/AuthValidationMessages.cs
@@ -2,7 +2,6 @@ namespace BooksApp.Application.Common.Constants.ValidationMessages;
 
 public static class AuthValidationMessages
 {
-    public const string NotFound = "User not found";
     public const string Occupied = "Email is occupied";
     public const string Password = "Password is wrong";
     public const string EmailOrPassword = "Email or password is wrong";
