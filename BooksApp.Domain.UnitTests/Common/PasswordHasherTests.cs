317de148ef2484b17432606a88e229a973d7fc49
Hosterip <hosteripwork@gmail.com>
2025-02-12 23:04:50 +0000

    password hasher tests

---
+++ BooksApp.Domain.UnitTests/Common/PasswordHasherTests.cs
@@ -0,0 +1,40 @@
+using BooksApp.Domain.Common.Interfaces;
+using BooksApp.Infrastructure.Authentication;
+using FluentAssertions;
+
+namespace BooksApp.Domain.UnitTests.Common;
+
+public class PasswordHasherTests
+{
+    private readonly IPasswordHasher _passwordHasher = new PasswordHasher();
+
+    [Fact]
+    public void IsValid_WhenHashAndSaltIsRight_ShouldReturnTrue()
+    {
+        // Arrange
+        var password = Guid
+            .NewGuid()
+            .ToString();
+        var hashSalt = _passwordHasher.GenerateHashSalt(password);
+
+        // Act
+        var result = _passwordHasher.IsPasswordValid(hashSalt.Hash, hashSalt.Salt, password);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void IsValid_WhenHashAndSaltInvalid_ShouldReturnFalse()
+    {
+        // Arrange
+        var password = string.Empty;
+        var hashSalt = (Hash: string.Empty, Salt: string.Empty);
+
+        // Act
+        var result = _passwordHasher.IsPasswordValid(hashSalt.Hash, hashSalt.Salt, password);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
