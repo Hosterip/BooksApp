573aadb15bfd22056375075aa10a9f98ccb7dce2
Hosterip <hosteripwork@gmail.com>
2025-02-13 20:17:26 +0100

    Domain book tests

---
+++ BooksApp.Domain.UnitTests/Books/BookTests.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using TestCommon.Common.Constants;
+using TestCommon.Genres;
+using TestCommon.Images;
+using TestCommon.Users;
+
+namespace BooksApp.Domain.UnitTests.Books;
+
+public class BookTests
+{
+    [Fact]
+    public void Create_WhenEverythingInOrder_ShouldCreateBook()
+    {
+        // Arrange
+        var genres = new List<Genre.Genre>
+        {
+            GenreFactory.CreateGenre()
+        };
+        var image = ImageFactory.CreateImage();
+        var user = UserFactory.CreateUser();
+        
+        // Act
+        var result = Book.Book.Create(
+            Constants.Books.Title,
+            Constants.Books.Description,
+            image,
+            user,
+            genres);
+
+        // Assert
+        result.Should().BeOfType<Book.Book>();
+        result.Title.Should().Be(Constants.Books.Title);
+    }
+}
