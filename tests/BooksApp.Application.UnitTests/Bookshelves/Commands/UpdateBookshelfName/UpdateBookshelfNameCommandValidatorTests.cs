46de6f769cf0b71d97405c61f45b80a8da220958
Hosterip <hosteripwork@gmail.com>
2025-03-10 23:33:19 +0100

    New tests

--- tests/BooksApp.Application.UnitTests/Bookshelves/Commands/UpdateBookshelfName/UpdateBookshelfNameCommandValidatorTests.cs
+++ tests/BooksApp.Application.UnitTests/Bookshelves/Commands/UpdateBookshelfName/UpdateBookshelfNameCommandValidatorTests.cs
@@ -1,4 +1,5 @@
 using BooksApp.Application.Bookshelves.Commands.UpdateBookshelfName;
+using BooksApp.Application.Common.Constants.ValidationMessages;
 using BooksApp.Application.Common.Interfaces;
 using FluentAssertions;
 using NSubstitute;
@@ -12,7 +13,7 @@ public class UpdateBookshelfNameCommandValidatorTests
     private readonly IUserService _userService = Substitute.For<IUserService>();
 
     [Fact]
-    public async Task Constructor_WhenEverythingIsOkay_ShouldNotThrowAnError()
+    public async Task ValidateAsync_WhenEverythingIsOkay_ShouldBeValid()
     {
         // Arrange
         var bookshelf = BookshelfFactory.CreateBookshelf();
@@ -31,5 +32,77 @@ public class UpdateBookshelfNameCommandValidatorTests
 
         // Assert
         result.IsValid.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task ValidateAsync_WhenBookshelfIsNotPossessedByAnUser_ShouldReturnSpecificErrorMessage()
+    {
+        // Arrange
+        var bookshelf = BookshelfFactory.CreateBookshelf();
+        
+        _unitOfWork.Bookshelves.AnyAsync(x => true).ReturnsForAnyArgs(false);
+        _unitOfWork.Bookshelves.GetSingleById(Guid.Empty).ReturnsForAnyArgs(bookshelf);
+
+        _userService.GetId().ReturnsForAnyArgs(Guid.Empty);
+
+        var updateBookshelfNameCommand = BookshelfCommandFactory
+            .CreateUpdateBookshelfNameCommand(bookshelfId: bookshelf.Id.Value);
+        var validator = new UpdateBookshelfNameCommandValidator(_unitOfWork, _userService);
+        
+        // Act
+        var result = await validator.ValidateAsync(updateBookshelfNameCommand);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Any(x => x.ErrorMessage == ValidationMessages.Bookshelf.NotYours).Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task ValidateAsync_WhenOldBookshelfAndNewBookshelfNameAreTheSame_ShouldReturnSpecificErrorMessage()
+    {
+        // Arrange
+        var bookshelf = BookshelfFactory.CreateBookshelf();
+        
+        _unitOfWork.Bookshelves.AnyAsync(x => true).ReturnsForAnyArgs(true);
+        _unitOfWork.Bookshelves.GetSingleById(Guid.Empty).ReturnsForAnyArgs(bookshelf);
+
+        _userService.GetId().ReturnsForAnyArgs(Guid.Empty);
+
+        var updateBookshelfNameCommand = BookshelfCommandFactory
+            .CreateUpdateBookshelfNameCommand(bookshelfId: bookshelf.Id.Value, newName: bookshelf.Name);
+        var validator = new UpdateBookshelfNameCommandValidator(_unitOfWork, _userService);
+        
+        // Act
+        var result = await validator.ValidateAsync(updateBookshelfNameCommand);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Any(x => x.ErrorMessage == ValidationMessages.Bookshelf.NameIsTheSameAsItWas).Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task ValidateAsync_WhenEveryErrorOccurs_ShouldTwoErrorMessages()
+    {
+        // Arrange
+        var bookshelf = BookshelfFactory.CreateBookshelf();
+        
+        _unitOfWork.Bookshelves.AnyAsync(x => true).ReturnsForAnyArgs(false);
+        _unitOfWork.Bookshelves.GetSingleById(Guid.Empty).ReturnsForAnyArgs(bookshelf);
+
+        _userService.GetId().ReturnsForAnyArgs(Guid.Empty);
+
+        var updateBookshelfNameCommand = BookshelfCommandFactory
+            .CreateUpdateBookshelfNameCommand(bookshelfId: bookshelf.Id.Value, newName: bookshelf.Name);
+        var validator = new UpdateBookshelfNameCommandValidator(_unitOfWork, _userService);
+        
+        // Act
+        var result = await validator.ValidateAsync(updateBookshelfNameCommand);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Any(x => x.ErrorMessage == ValidationMessages.Bookshelf.NotYours)
+            .Should().BeTrue();
+        result.Errors.Any(x => x.ErrorMessage == ValidationMessages.Bookshelf.NameIsTheSameAsItWas)
+            .Should().BeTrue();
     }
 }
