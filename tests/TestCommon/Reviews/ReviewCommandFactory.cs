bb9ce624c4ba3bf3dc244205db46493b66f0132b
Hosterip <hosteripwork@gmail.com>
2025-04-23 07:54:30 +0200

    Review command factory

---
+++ tests/TestCommon/Reviews/ReviewCommandFactory.cs
@@ -0,0 +1,54 @@
+using BooksApp.Application.Reviews.Commands.CreateReview;
+using BooksApp.Application.Reviews.Commands.DeleteReview;
+using BooksApp.Application.Reviews.Commands.PrivilegedDeleteReview;
+using BooksApp.Application.Reviews.Commands.UpdateReview;
+using TestCommon.Common.Constants;
+
+namespace TestCommon.Reviews;
+
+public static class ReviewCommandFactory
+{
+    public static CreateReviewCommand CreateCreateReviewCommand(
+        int rating = Constants.Reviews.Rating,
+        string body = Constants.Reviews.Body,
+        Guid? bookId = null)
+    {
+        return new CreateReviewCommand
+        {
+            Rating = rating,
+            Body = body,
+            BookId = bookId ?? Guid.NewGuid()
+        };
+    }
+    
+    public static UpdateReviewCommand CreateUpdateReviewCommand(
+            int rating = Constants.Reviews.Rating,
+            string body = Constants.Reviews.Body,
+            Guid? reviewId = null)
+        {
+            return new UpdateReviewCommand
+            {
+                Rating = rating,
+                Body = body,
+                ReviewId = reviewId ?? Guid.NewGuid()
+            };
+        }
+
+    public static DeleteReviewCommand CreateDeleteReviewCommand(
+        Guid? reviewId = null)
+    {
+        return new DeleteReviewCommand
+        {
+            ReviewId = reviewId ?? Guid.NewGuid()
+        };
+    }
+
+    public static PrivilegedDeleteReviewCommand CreatePrivilegedDeleteReviewCommand(
+        Guid? reviewId = null)
+    {
+        return new PrivilegedDeleteReviewCommand
+        {
+            ReviewId = reviewId ?? Guid.NewGuid()
+        };
+    }
+}
